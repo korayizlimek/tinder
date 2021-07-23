d104bbf15aab4240d064503077cd660f3e1ffc1e
korayizlimek <korayizlimek@gmail.com>
2021-07-23 17:58:16 +0300

    fix login screen

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,95 +11,91 @@ import IconButton from "@material-ui/core/IconButton";
 import { Link } from "react-router-dom";
 
 function Card({
-    id,
-    avatarUrl,
-    name,
-    status,
-    species,
-    gender,
-    type,
-    location,
+  id,
+  avatarUrl,
+  name,
+  status,
+  species,
+  gender,
+  type,
+  location,
 }) {
-    const [favorite, setFavorite] = useState(false);
-    const [star, setStar] = useState(false);
-    const [flash, setFlash] = useState(false);
+  const [favorite, setFavorite] = useState(false);
+  const [star, setStar] = useState(false);
+  const [flash, setFlash] = useState(false);
 
-    let cardClassName;
-    if (status === "Alive") {
-        cardClassName = "card-alive";
-    } else if (status === "Dead") {
-        cardClassName = "card-dead";
-    } else {
-        cardClassName = "";
-    }
+  let cardClassName;
+  if (status === "Alive") {
+    cardClassName = "card-alive";
+  } else if (status === "Dead") {
+    cardClassName = "card-dead";
+  } else {
+    cardClassName = "";
+  }
 
-    return (
-        <div className={`card ${cardClassName}`}>
-            <Link to={`/detail/${id}`}>
-                <img className="card-image" src={avatarUrl} alt="avatar" />
+  return (
+    <div className={`card ${cardClassName}`}>
+      <Link to={`/detail/${id}`}>
+        <img className="card-image" src={avatarUrl} alt="avatar" />
 
-                {name !== "" && (
-                    <p>
-                        <span className="card-title">Name:</span> {name}
-                    </p>
-                )}
+        {name !== "" && (
+          <p>
+            <span className="card-title">Name:</span> {name}
+          </p>
+        )}
 
-                {status !== "" && (
-                    <p>
-                        <span className="card-title">Status:</span>{" "}
-                        {status}
-                    </p>
-                )}
-                {species !== "" && (
-                    <p>
-                        <span className="card-title">Species:</span>{" "}
-                        {species}{" "}
-                    </p>
-                )}
-                {gender !== "" && (
-                    <p>
-                        <span className="card-title">Gender:</span>{" "}
-                        {gender}
-                    </p>
-                )}
-                {type !== "" && (
-                    <p>
-                        <span className="card-title">Type:</span> {type}{" "}
-                    </p>
-                )}
-                {location !== "" && (
-                    <p>
-                        <span className="card-title">Location:</span>{" "}
-                        {location}{" "}
-                    </p>
-                )}
-            </Link>
-            <div className="card-buttons">
-                <IconButton onClick={() => setStar(!star)}>
-                    <StarRateIcon
-                        className={`card-button-icon ${
-                            star ? "card-button-icon-active" : ""
-                        }`}
-                    />
-                </IconButton>
-                <IconButton onClick={() => setFavorite(!favorite)}>
-                    <FavoriteIcon
-                        className={`card-button-icon ${
-                            favorite ? "card-button-icon-active" : ""
-                        }`}
-                    />
-                </IconButton>
-                <IconButton onClick={() => setFlash(!flash)}>
-                    <FlashOnIcon
-                        className={`card-button-icon ${
-                            flash ? "card-button-icon-active" : ""
-                        }`}
-                    />
-                </IconButton>
-            </div>
-        </div>
-    );
-    // Karaktere tıklandığı router ile yeni bir sayfa açılsın ve karakter kartındaki bilgilerin aynısı ve son oynadığı 5 bölümün adlarını görüntüleyebilelim.
+        {status !== "" && (
+          <p>
+            <span className="card-title">Status:</span> {status}
+          </p>
+        )}
+        {species !== "" && (
+          <p>
+            <span className="card-title">Species:</span> {species}{" "}
+          </p>
+        )}
+        {gender !== "" && (
+          <p>
+            <span className="card-title">Gender:</span> {gender}
+          </p>
+        )}
+        {type !== "" && (
+          <p>
+            <span className="card-title">Type:</span> {type}{" "}
+          </p>
+        )}
+        {location !== "" && (
+          <p>
+            <span className="card-title">Location:</span> {location}{" "}
+          </p>
+        )}
+      </Link>
+      <div className="card-buttons">
+        <IconButton onClick={() => setStar(!star)}>
+          <StarRateIcon
+            className={`card-button-icon ${
+              star ? "card-button-icon-active" : ""
+            }`}
+          />
+        </IconButton>
+        <IconButton onClick={() => setFavorite(!favorite)}>
+          <FavoriteIcon
+            className={`card-button-icon ${
+              favorite ? "card-button-icon-active" : ""
+            }`}
+          />
+        </IconButton>
+        <IconButton onClick={() => setFlash(!flash)}>
+          <FlashOnIcon
+            className={`card-button-icon ${
+              flash ? "card-button-icon-active" : ""
+            }`}
+          />
+        </IconButton>
+      </div>
+    </div>
+  );
+  // Karaktere tıklandığı router ile yeni bir sayfa açılsın ve karakter kartındaki bilgilerin aynısı ve son oynadığı 5 bölümün adlarını görüntüleyebilelim.
 }
 
 export default Card;
--- src/components/CardDetail.js
+++ src/components/CardDetail.js
@@ -12,130 +12,114 @@ import { IconButton } from "@material-ui/core";
 import Episode from "./Episode";
 
 function CardDetail(props) {
-    const params = useParams().character;
+  const params = useParams().character;
 
-    const [character, setCharacter] = useState({});
+  const [character, setCharacter] = useState({});
 
-    const [favorite, setFavorite] = useState(false);
-    const [star, setStar] = useState(false);
-    const [flash, setFlash] = useState(false);
+  const [favorite, setFavorite] = useState(false);
+  const [star, setStar] = useState(false);
+  const [flash, setFlash] = useState(false);
 
-    useEffect(() => {
-        axios
-            .get(`https://rickandmortyapi.com/api/character/${params}`)
-            .then((res) => getCharacter(res.data))
-            .catch((err) => console.log("Cart detail error: ", err));
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`https://rickandmortyapi.com/api/character/${params}`)
+      .then((res) => getCharacter(res.data))
+      .catch((err) => console.log("Cart detail error: ", err));
+  }, []);
 
-    const getCharacter = (characters) => {
-        let lastEpisodes = characters.episode.reverse().slice(0, 5);
-        characters.episode = lastEpisodes;
-        setCharacter(characters);
-    };
+  const getCharacter = (characters) => {
+    let lastEpisodes = characters.episode.reverse().slice(0, 5);
+    characters.episode = lastEpisodes;
+    setCharacter(characters);
+  };
 
-    return (
-        <div className="cardDetail">
-            <div className="cardDetail-image-section">
-                <img
-                    className="cardDetail-image"
-                    src={character.image}
-                    alt="CharacterDetailImage"
-                />
-            </div>
-            <div className="cardDetail-descriptions-container">
-                <div className="cardDetail-description">
-                    <p className="cardDetail-description-title">Name :</p>
-                    <p className="cardDetail-description-value">
-                        {character.name}
-                    </p>
-                </div>
-                <div className="cardDetail-description">
-                    <p className="cardDetail-description-title">
-                        Status :
-                    </p>
-                    <p className="cardDetail-description-value">
-                        {character.status}
-                    </p>
-                </div>
-                <div className="cardDetail-description">
-                    <p className="cardDetail-description-title">
-                        Species :
-                    </p>
-                    <p className="cardDetail-description-value">
-                        {character.species}
-                    </p>
-                </div>
-                {character.type && (
-                    <div className="cardDetail-description">
-                        <p className="cardDetail-description-title">
-                            Type :
-                        </p>
-                        <p className="cardDetail-description-value">
-                            {character.type}
-                        </p>
-                    </div>
-                )}
-
-                <div className="cardDetail-description">
-                    <p className="cardDetail-description-title">
-                        Gender :
-                    </p>
-                    <p className="cardDetail-description-value">
-                        {character.gender}
-                    </p>
-                </div>
-                <div className="cardDetail-description">
-                    <p className="cardDetail-description-title">
-                        Location :
-                    </p>
-                    <p className="cardDetail-description-value">
-                        {character.location?.name}
-                    </p>
-                </div>
-                <div className="cardDetail-description">
-                    <p className="cardDetail-description-title">
-                        Origin :
-                    </p>
-                    <p className="cardDetail-description-value">
-                        {character.origin?.name}
-                    </p>
-                </div>
-                <div className="cardDetail-description">
-                    <p className="cardDetail-description-title">
-                        Episodes :
-                    </p>
-                    <p className="cardDetail-description-value">
-                        {character.episode?.map((episode) => (
-                            <Episode episode={episode} />
-                        ))}
-                    </p>
-                </div>
-            </div>
-            <div className="cardDetail-buttons">
-                <IconButton onClick={() => setStar(!star)}>
-                    <StarRateIcon
-                        className={`card-button-icon ${
-                            star ? "card-button-icon-active" : ""
-                        }`}
-                    />
-                </IconButton>
-                <IconButton onClick={() => setFavorite(!favorite)}>
-                    <FavoriteIcon
-                        className={`card-button-icon ${
-                            favorite ? "card-button-icon-active" : ""
-                        }`}
-                    />
-                </IconButton>
-                <IconButton onClick={() => setFlash(!flash)}>
-                    <FlashOnIcon
-                        className={`card-button-icon ${
-                            flash ? "card-button-icon-active" : ""
-                        }`}
-                    />
-                </IconButton>
-            </div>
+  return (
+    <div className="cardDetail">
+      <div className="cardDetail-image-section">
+        <img
+          className="cardDetail-image"
+          src={character.image}
+          alt="CharacterDetailImage"
+        />
+      </div>
+      <div className="cardDetail-descriptions-container">
+        <div className="cardDetail-description">
+          <p className="cardDetail-description-title">Name :</p>
+          <p className="cardDetail-description-value">{character.name}</p>
         </div>
-    );
+        <div className="cardDetail-description">
+          <p className="cardDetail-description-title">Status :</p>
+          <p className="cardDetail-description-value">
+            {character.status}
+          </p>
+        </div>
+        <div className="cardDetail-description">
+          <p className="cardDetail-description-title">Species :</p>
+          <p className="cardDetail-description-value">
+            {character.species}
+          </p>
+        </div>
+        {character.type && (
+          <div className="cardDetail-description">
+            <p className="cardDetail-description-title">Type :</p>
+            <p className="cardDetail-description-value">
+              {character.type}
+            </p>
+          </div>
+        )}
+
+        <div className="cardDetail-description">
+          <p className="cardDetail-description-title">Gender :</p>
+          <p className="cardDetail-description-value">
+            {character.gender}
+          </p>
+        </div>
+        <div className="cardDetail-description">
+          <p className="cardDetail-description-title">Location :</p>
+          <p className="cardDetail-description-value">
+            {character.location?.name}
+          </p>
+        </div>
+        <div className="cardDetail-description">
+          <p className="cardDetail-description-title">Origin :</p>
+          <p className="cardDetail-description-value">
+            {character.origin?.name}
+          </p>
+        </div>
+        <div className="cardDetail-description">
+          <p className="cardDetail-description-title">Episodes :</p>
+          <p className="cardDetail-description-value">
+            {character.episode?.map((episode) => (
+              <Episode episode={episode} />
+            ))}
+          </p>
+        </div>
+      </div>
+      <div className="cardDetail-buttons">
+        <IconButton onClick={() => setStar(!star)}>
+          <StarRateIcon
+            className={`card-button-icon ${
+              star ? "card-button-icon-active" : ""
+            }`}
+          />
+        </IconButton>
+        <IconButton onClick={() => setFavorite(!favorite)}>
+          <FavoriteIcon
+            className={`card-button-icon ${
+              favorite ? "card-button-icon-active" : ""
+            }`}
+          />
+        </IconButton>
+        <IconButton onClick={() => setFlash(!flash)}>
+          <FlashOnIcon
+            className={`card-button-icon ${
+              flash ? "card-button-icon-active" : ""
+            }`}
+          />
+        </IconButton>
+      </div>
+    </div>
+  );
 }
 
 export default CardDetail;
