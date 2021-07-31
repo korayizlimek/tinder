75a45db44957e671bb2882fd746628b611cad905
korayizlimek <korayizlimek@gmail.com>
2021-07-31 18:24:11 +0300

    add filter and do refactoring

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,161 +1,127 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Filter.css";
 
 // Icons
 import WcIcon from "@material-ui/icons/Wc";
 import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
 import RedditIcon from "@material-ui/icons/Reddit";
+import { useDispatch } from "react-redux";
+import { getCharacters } from "../redux/actions";
 
-function Filter({
-    setFilterStatus: setCardFilterStatus,
-    setFilterSpecies: setCardFilterSpecies,
-    setFilterGender: setCardFilterGender,
-}) {
-    const [filterStatus, setFilterStatus] = useState("");
-    const [filterSpecies, setFilterSpecies] = useState("");
-    const [filterGender, setFilterGender] = useState("");
+function Filter({ currentPage }) {
+  const [filterStatus, setFilterStatus] = useState("");
+  const [filterSpecies, setFilterSpecies] = useState("");
+  const [filterGender, setFilterGender] = useState("");
 
-    const classNameFilterStatus = (value) => {
-        let className = "btn ";
-        if (filterStatus === value) {
-            className += "button-active";
-        }
-        return className;
-    };
+  const dispatch = useDispatch();
 
-    const classNameFilterSpecies = (value) => {
-        let className = "btn ";
-        if (filterSpecies === value) {
-            className += "button-active";
-        }
-        return className;
-    };
-    const classNameFilterGender = (value) => {
-        let className = "btn ";
-        if (filterGender === value) {
-            className += "button-active";
-        }
-        return className;
-    };
-
-    const handleFilterStatus = (e) => {
-        setFilterStatus(e.target.value);
-        setCardFilterStatus(e.target.value);
-    };
-    const handleFilterSpecies = (e) => {
-        setFilterSpecies(e.target.value);
-        setCardFilterSpecies(e.target.value);
-    };
-    const handleFilterGender = (e) => {
-        setFilterGender(e.target.value);
-        setCardFilterGender(e.target.value);
-    };
-
-    return (
-        <div className="filter">
-            <div className="cards-filters">
-                <div className="btns">
-                    <LocalHospitalIcon
-                        fontSize="large"
-                        color="secondary"
-                    />
-                    <p>Status:</p>
-                    <button
-                        value=""
-                        className={classNameFilterStatus("")}
-                        onClick={(e) => handleFilterStatus(e)}
-                    >
-                        Show All
-                    </button>
-                    <button
-                        value="alive"
-                        className={classNameFilterStatus("alive")}
-                        onClick={(e) => handleFilterStatus(e)}
-                    >
-                        Alive
-                    </button>
-                    <button
-                        value="dead"
-                        className={classNameFilterStatus("dead")}
-                        onClick={(e) => handleFilterStatus(e)}
-                    >
-                        Dead
-                    </button>
-                    <button
-                        value="unknown"
-                        className={classNameFilterStatus("unknown")}
-                        onClick={(e) => handleFilterStatus(e)}
-                    >
-                        Unknown
-                    </button>
-                </div>
-                <div className="btns">
-                    <RedditIcon fontSize="large" color="secondary" />
-                    <p>Species:</p>
-                    <button
-                        value=""
-                        className={classNameFilterSpecies("")}
-                        onClick={(e) => handleFilterSpecies(e)}
-                    >
-                        Show All
-                    </button>
-                    <button
-                        value="human"
-                        className={classNameFilterSpecies("human")}
-                        onClick={(e) => handleFilterSpecies(e)}
-                    >
-                        Human
-                    </button>
-                    <button
-                        value="alien"
-                        className={classNameFilterSpecies("alien")}
-                        onClick={(e) => handleFilterSpecies(e)}
-                    >
-                        Alien
-                    </button>
-                </div>
-                <div className="btns">
-                    <WcIcon fontSize="large" color="secondary" />
-                    <p>Gender:</p>
-                    <button
-                        value=""
-                        className={classNameFilterGender("")}
-                        onClick={(e) => handleFilterGender(e)}
-                    >
-                        Show All
-                    </button>
-                    <button
-                        value="male"
-                        className={classNameFilterGender("male")}
-                        onClick={(e) => handleFilterGender(e)}
-                    >
-                        Male
-                    </button>
-                    <button
-                        value="female"
-                        className={classNameFilterGender("female")}
-                        onClick={(e) => handleFilterGender(e)}
-                    >
-                        Female
-                    </button>
-                    <button
-                        value="genderless"
-                        className={classNameFilterGender("genderless")}
-                        onClick={(e) => handleFilterGender(e)}
-                    >
-                        Genderless
-                    </button>
-                    <button
-                        value="unknown"
-                        className={classNameFilterGender("unknown")}
-                        onClick={(e) => handleFilterGender(e)}
-                    >
-                        Unknown
-                    </button>
-                </div>
-            </div>
-        </div>
+  useEffect(() => {
+    const resetCurrentPage = 1;
+    dispatch(
+      getCharacters(
+        resetCurrentPage,
+        filterStatus,
+        filterGender,
+        filterSpecies
+      )
     );
+  }, [filterStatus, filterSpecies, filterGender]);
+
+  useEffect(() => {
+    console.log("new", currentPage);
+    dispatch(
+      getCharacters(currentPage, filterStatus, filterGender, filterSpecies)
+    );
+  }, [currentPage]);
+
+  const classNameFilterStatus = (value) => {
+    let className = "btn ";
+    if (filterStatus === value) {
+      className += "button-active";
+    }
+    return className;
+  };
+
+  const classNameFilterSpecies = (value) => {
+    let className = "btn ";
+    if (filterSpecies === value) {
+      className += "button-active";
+    }
+    return className;
+  };
+  const classNameFilterGender = (value) => {
+    let className = "btn ";
+    if (filterGender === value) {
+      className += "button-active";
+    }
+    return className;
+  };
+
+  const handleFilterStatus = (e) => {
+    setFilterStatus(e.target.value);
+  };
+  const handleFilterSpecies = (e) => {
+    setFilterSpecies(e.target.value);
+  };
+  const handleFilterGender = (e) => {
+    setFilterGender(e.target.value);
+  };
+
+  const statusButtonsNames = ["", "Alive", "Dead", "Unknown"];
+  const speciesButtonsNames = ["", "Human", "Alien"];
+  const genderButtonsNames = [
+    "",
+    "Male",
+    "Female",
+    "Genderless",
+    "Unknown",
+  ];
+
+  return (
+    <div className="filter">
+      <div className="cards-filters">
+        <div className="btns">
+          <LocalHospitalIcon fontSize="large" color="secondary" />
+          <p>Status:</p>
+          {statusButtonsNames.map((element) => (
+            <button
+              value={element}
+              className={classNameFilterStatus(element)}
+              onClick={(e) => handleFilterStatus(e)}
+            >
+              {element || "Show All"}
+            </button>
+          ))}
+        </div>
+        <div className="btns">
+          <RedditIcon fontSize="large" color="secondary" />
+          <p>Species:</p>
+          {speciesButtonsNames.map((element) => (
+            <button
+              value={element}
+              className={classNameFilterSpecies(element)}
+              onClick={(e) => handleFilterSpecies(e)}
+            >
+              {element || "Show All"}
+            </button>
+          ))}
+        </div>
+        <div className="btns">
+          <WcIcon fontSize="large" color="secondary" />
+          <p>Gender:</p>
+          {genderButtonsNames.map((element) => (
+            <button
+              value={element}
+              className={classNameFilterGender(element)}
+              onClick={(e) => handleFilterGender(e)}
+            >
+              {element || "Show All"}
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Filter;
