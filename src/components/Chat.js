231155aec682d024e002bd1cd4de8d6389fb53ef
korayizlimek <korayizlimek@gmail.com>
2021-07-31 18:27:24 +0300

    change new file structure for refactoring

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,24 +3,20 @@ import { Link } from "react-router-dom";
 import "./Chat.css";
 
 function Chat({ imageUri, name, message, timestamp }) {
-    return (
-        <Link to={`/chat/${name}`}>
-            <div className="chat">
-                <div className="chat-image">
-                    <img
-                        className="chat-image"
-                        src={imageUri}
-                        alt="ProfileImage"
-                    />
-                </div>
-                <div className="chat-details">
-                    <p className="chat-details-title">{name}</p>
-                    <p className="chat-details-message">{message}</p>
-                </div>
-                <p className="chat-timestamp">{timestamp}</p>
-            </div>
-        </Link>
-    );
+  return (
+    <Link to={`/chat/${name}`}>
+      <div className="chat">
+        <div className="chat-image-section">
+          <img className="chat-image" src={imageUri} alt="ProfileImage" />
+        </div>
+        <div className="chat-details">
+          <p className="chat-details-title">{name}</p>
+          <p className="chat-details-message">{message}</p>
+        </div>
+        <p className="chat-timestamp">{timestamp}</p>
+      </div>
+    </Link>
+  );
 }
 
 export default Chat;
--- src/components/Episode.js
+++ src/components/Episode.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { getEpisode } from "../services/api";
 
 function Episode({ episode }) {
   const [episodeName, setEpisodeName] = useState();
   useEffect(() => {
-    axios
-      .get(episode)
-      .then((res) => setEpisodeName(res.data.name))
-      .catch((err) => console.log("Episode Card Detail Error : ", err));
-    axios.get();
-    console.log(episode);
+    // axios
+    //   .get(episode)
+    //   .then((res) => setEpisodeName(res.data.name))
+    //   .catch((err) => console.log("Episode Card Detail Error : ", err));
+    // axios.get();
+    // console.log(episode);
+    getEpisode(episode);
   }, [episode]);
 
   return <div>{episodeName}</div>;
